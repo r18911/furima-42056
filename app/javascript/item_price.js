fc517eb55b35a3ae23db8f66390af85f6ec8bc9f
r18911 <ryousuke18911@icloud.com>
2025-02-27 22:26:12 +0900

    手順2~5

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,15 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = inputValue - tax;
+
+    addTaxDom.innerHTML = tax;
+    profitDom.innerHTML = profit;
+  });
 });
