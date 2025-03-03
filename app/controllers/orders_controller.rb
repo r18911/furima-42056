cd0d85221c859ff723a4c0910a35caaf57fb925b
r18911 <ryousuke18911@icloud.com>
2025-03-03 14:20:21 +0900

    STEP1 手順1,2

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,7 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root to: "items#index"
-  resources :items
+  resources :items do
+    resources :orders
 end
