4f02e7ff834e679fccace8e63a764b490eb1e9e0
r18911 <ryousuke18911@icloud.com>
2025-03-04 19:26:51 +0900

    STEP2 手順5まで

--- app/javascript/pay.js
+++ app/javascript/pay.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_a1c9044943c0b26c8c3f3be4')// PAY.JPテスト公開鍵
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -29,3 +30,4 @@ const pay = () => {
 };
 
 window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
